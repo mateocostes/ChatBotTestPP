9c0c9ecfcd13669c3e952d9eb9785203b7118d6d
mateocostes <mateocostes@gmail.com>
2023-04-17 18:33:04 -0300

    Mejora preguntas v3

--- actions/actions.py
+++ actions/actions.py
@@ -45,13 +45,15 @@ api_endpoint_get_vector = "http://201.235.167.187:8088/dispatcher/get-vector" # 
 diccionarioParticipantes = ""
 #Variables utilizadas para guardar los valores
 pregunta_actual = 0
-valor_riesgo = 0
-valor_optimismo = 0
-valor_adaptabilidad = 0
+valores_categorias = []
 habilidades = []
 lenguajes = []
 direcPreguntas = "actions/preguntas.json"
 diccionarioPreguntas = readArchivo(direcPreguntas)
+categoriasPreguntas = []
+categoria_actual = 0
+valor_pregunta = 0
+habilidades_lenguajes = 0
 
 def existeParticipante(nombre_participante) -> bool:
     response = requests.get(url=api_endpoint_get_vector).text
@@ -62,74 +64,96 @@ def existeParticipante(nombre_participante) -> bool:
             return True
     return False
 
-def crearVector(riesgo, optimismo, adaptabilidad, habilidades, lenguajes):
+def crearVector():
+    global habilidades, lenguajes, categoriasPreguntas
     vector = {}
-    vector["riesgo"] = riesgo
-    vector["optimismo"] = optimismo
-    vector["adaptabilidad"] = adaptabilidad
+    for i in range(len(categoriasPreguntas)):
+        vector[categoriasPreguntas[i]] = valores_categorias[i]
     vector["habilidades"] = habilidades
     vector["lenguajes"] = lenguajes
     return vector
 
-def altaValores(nombre_partipante):
-    global valor_riesgo, valor_optimismo, valor_adaptabilidad, habilidades, lenguajes
+def altaValores(nombre_partipante) -> bool:
+    global habilidades, lenguajes
     data = {}
     data ["agilebotId"] = nombre_partipante
-    data ["vector"] = crearVector(round(valor_riesgo/cantidadPreguntas("riesgo")), round(valor_optimismo/cantidadPreguntas("optimismo")), round(valor_adaptabilidad/cantidadPreguntas("adaptabilidad")), habilidades, lenguajes) #Casteo el valor a entero y lo divido por la cantidad de preguntas
+    data ["vector"] = crearVector() #Casteo el valor a entero y lo divido por la cantidad de preguntas
     r = requests.post(url=api_endpoint_set_vector, json=data)
+    return r.ok #chequear
     #print(data)
     #response = requests.get(url=api_endpoint_get_vector).text
     #print("response: " + response)
 
-def darAlta(nombre_participante) -> bool:
-    global pregunta_actual
-    if (pregunta_actual == preguntasTotales("habilidades") - 1): #Chequeo si se realizaron de forma correcta todas la preguntas para asi dar de alta
-        altaValores(nombre_participante)
-        return true
-    else:
-        return false
-
-def leerPregunta(categoria, posicion, dispatcher):
-    message = str(diccionarioPreguntas[categoria][posicion])
-    dispatcher.utter_message(text=message)
-
 def cantidadPreguntas(categoria) -> int:
-    #print(str(len(diccionarioPreguntas[categoria])))
+    return (len(diccionarioPreguntas["preguntas"][categoria]))
+
+def cantidadRegistros(categoria) -> int:
     return (len(diccionarioPreguntas[categoria]))
 
-def leerTotalPreguntas(categoria, dispatcher):
-    for pregunta in diccionarioPreguntas[categoria]:
+def leerIntroduccion(dispatcher):
+    for pregunta in diccionarioPreguntas["introduccion"]:
         message = str(pregunta)
         dispatcher.utter_message(text=message)
 
-def preguntasTotales(categoriaFinal) -> int:
-    cantidad = 0
-    for keys in diccionarioPreguntas.keys():
-        if not (str(keys).__eq__("introduccion")):
-            cantidad = cantidad + cantidadPreguntas(keys)
-            if str(keys).__eq__(str(categoriaFinal)):
-                return cantidad
+def obtenerPregunta(categoria, posicion) -> Text:
+    return str(diccionarioPreguntas["preguntas"][categoria][posicion])
         
-def obtenerHabilidad(posicion) -> Text:
-    return(str(diccionarioPreguntas["habilidades"][posicion]))
+def obtenerRegistro(categoria, posicion) -> Text:
+    return(str(diccionarioPreguntas[categoria][posicion]))
 
-def accionConocimiento(guarda, dispatcher, tracker):
-    global habilidades, lenguajes, pregunta_actual
-    nombre_participante = str(tracker.get_slot("participante"))
-    message = ""
-    print("pregunta_actual: " + str(pregunta_actual))
-    print("preguntasTotales: " + str(preguntasTotales("habilidades")))
-    if bool(guarda):
-        habilidades.append(obtenerHabilidad(pregunta_actual - preguntasTotales("adaptabilidad")))
-    if (pregunta_actual < preguntasTotales("habilidades") - 1):
+def accionConocimiento(guarda, nombre_participante) -> Text:
+    global habilidades, lenguajes, pregunta_actual, habilidades_lenguajes
+    if habilidades_lenguajes == 0:
+        if bool(guarda):
+            habilidades.append(obtenerRegistro("habilidades", pregunta_actual))
         pregunta_actual = pregunta_actual + 1
-        leerPregunta("habilidades", pregunta_actual - preguntasTotales("adaptabilidad"), dispatcher)
-    elif (pregunta_actual == preguntasTotales("habilidades") - 1):
-        if darAlta(nombre_participante):
-            message = nombre_participante + ", realizaste de forma correcta la prueba de aptitud! Muchas gracias y hasta luego!"
-        else: message = "Ocurrio un error al realizar la prueba de aptitud! Vuelva a intentarlo de nuevo."
-        dispatcher.utter_message(text=message)
+        if (pregunta_actual < cantidadRegistros("habilidades")):
+            return generarPreguntaHabilidades(obtenerRegistro("habilidades", pregunta_actual))
+        else: 
+            habilidades_lenguajes = 1
+            pregunta_actual = 0
+            return generarPreguntaLenguajes(obtenerRegistro("lenguajes", pregunta_actual))
+    else:
+        if bool(guarda):
+            lenguajes.append(obtenerRegistro("lenguajes", pregunta_actual))
+        pregunta_actual = pregunta_actual + 1
+        if (pregunta_actual < cantidadRegistros("lenguajes")):
+            return generarPreguntaHabilidades(obtenerRegistro("lenguajes", pregunta_actual))
+        else: 
+            if altaValores(nombre_participante):
+                return (nombre_participante + ", realizaste de forma correcta la prueba de aptitud! Muchas gracias y hasta luego!")
+            else: return ("Ocurrio un error al realizar la prueba de aptitud! Vuelva a intentarlo de nuevo.")
 
+
+def generarPreguntaHabilidades(habilidad) -> Text:
+    lista_preguntas = []
+    pregunta1 = "¿Trabajaste con " + habilidad + "?"
+    pregunta2 = "¿Tenes conocimientos de " + habilidad + "?"
+    pregunta3 = "¿Realizaste algun trabajo de " + habilidad + "?"
+    pregunta4 = "¿Alguna vez hiciste algo con " + habilidad + "?"
+    pregunta5 = "¿Tenes habilidades de " + habilidad + "?"
+    #AGREGAR MAS EJEMPLOS
+    lista_preguntas.append(pregunta1)
+    lista_preguntas.append(pregunta2)
+    lista_preguntas.append(pregunta3)
+    lista_preguntas.append(pregunta4)
+    lista_preguntas.append(pregunta5)
+    return random.choice(lista_preguntas)
+
+def generarPreguntaLenguajes(lenguaje) -> Text:
+    lista_preguntas = []
+    pregunta1 = "¿Te sentis comodo programando en " + lenguaje + "?"
+    pregunta2 = "¿Sabes programar en " + lenguaje + "?"
+    pregunta3 = "¿Te gusta programar en " + lenguaje + "?"
+    pregunta4 = "¿Tenés conocimientos de " + lenguaje + "?"
+    pregunta5 = "¿Programas en " + lenguaje + "?"
+    #AGREGAR MAS EJEMPLOS
+    lista_preguntas.append(pregunta1)
+    lista_preguntas.append(pregunta2)
+    lista_preguntas.append(pregunta3)
+    lista_preguntas.append(pregunta4)
+    lista_preguntas.append(pregunta5)
+    return random.choice(lista_preguntas)
 
 class ActionGuardarNombre(Action):
 
@@ -141,16 +165,16 @@ class ActionGuardarNombre(Action):
         message = ""
         if (nombre_partipante != None):
             #Reinicio las varibles globales
-            global pregunta_actual, valor_riesgo, valor_optimismo, valor_adaptabilidad, habilidades, lenguajes
+            global pregunta_actual, habilidades, lenguajes, categoriasPreguntas, categoria_actual
             pregunta_actual = 0
-            valor_riesgo = 0
-            valor_optimismo = 0
-            valor_adaptabilidad = 0
             habilidades = []
             lenguajes = []
+            categoriasPreguntas = list(dict(diccionarioPreguntas["preguntas"]).keys())
+            print("categorias preguntas: " + str(categoriasPreguntas))
             if(existeParticipante(nombre_partipante)):
-                leerTotalPreguntas("introduccion", dispatcher)
-                leerPregunta("riesgo", 0, dispatcher)
+                leerIntroduccion(dispatcher)
+                message = obtenerPregunta(categoriasPreguntas[categoria_actual], 0) #Se sabe que la categoria tiene preguntas
+                dispatcher.utter_message(text=message)
             else:
                 message = "El nombre no corresponde a un AgileBot perteneciente al mundo"
                 dispatcher.utter_message(text=message)
@@ -164,26 +188,28 @@ class ActionGuardarValorRespuesta(Action):
         return "guardar_valor_respuesta"
 
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        global valor_riesgo, valor_optimismo, valor_adaptabilidad, habilidades, lenguajes, pregunta_actual #Declaro las variables globales a utilizar
+        global habilidades, lenguajes, pregunta_actual, categoria_actual, valor_pregunta #Declaro las variables globales a utilizar
         valor_respuesta = next (tracker.get_latest_entity_values("valor_respuesta"),None)
-        message = "Error"
+        message = "Error"        
         if (valor_respuesta != None):
-            valor_respuesta = int(valor_respuesta)
+            valor_pregunta = valor_pregunta + int(valor_respuesta)
             pregunta_actual = pregunta_actual + 1
-            if (pregunta_actual < cantidadPreguntas("riesgo")):
-                valor_riesgo = valor_riesgo + valor_respuesta
-                print(str(valor_riesgo))
-                leerPregunta("riesgo", pregunta_actual, dispatcher)
-            elif (pregunta_actual < preguntasTotales("optimismo")):
-                valor_optimismo = valor_optimismo + valor_respuesta
-                leerPregunta("optimismo", pregunta_actual - cantidadPreguntas("riesgo"), dispatcher)
-            elif (pregunta_actual < preguntasTotales("adaptabilidad")):
-                valor_adaptabilidad = valor_adaptabilidad + valor_respuesta
-                leerPregunta("adaptabilidad", pregunta_actual - preguntasTotales("optimismo"), dispatcher)
-            else : 
-                message = "En las siguiente preguntas responde con Si o No"
+            if pregunta_actual < cantidadPreguntas(categoriasPreguntas[categoria_actual]): #Si seguimos en la misma categoria
+                message = obtenerPregunta(categoriasPreguntas[categoria_actual], pregunta_actual)
                 dispatcher.utter_message(text=message)
-                leerPregunta("habilidades", 0, dispatcher)
+            else:
+                valores_categorias.append(round(valor_pregunta/cantidadPreguntas(categoriasPreguntas[categoria_actual])))
+                pregunta_actual = 0 #para seguir con las preguntas de la proxima categoria
+                categoria_actual = categoria_actual + 1
+                if categoria_actual < len(categoriasPreguntas):
+                    valor_pregunta = 0
+                    message = obtenerPregunta(categoriasPreguntas[categoria_actual], pregunta_actual)
+                    dispatcher.utter_message(text=message)
+                else: 
+                    message = "En las siguientes preguntas respondé con Si o No"
+                    dispatcher.utter_message(text=message)
+                    message = generarPreguntaHabilidades(obtenerRegistro("habilidades", pregunta_actual))
+                    dispatcher.utter_message(text=message)
         else:
             message = "No se reconocio el valor de la respuesta"   
             dispatcher.utter_message(text=message)
@@ -194,7 +220,9 @@ class ActionGuardarConocimiento(Action):
         return "guardar_conocimiento"
 
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        accionConocimiento(True, dispatcher, tracker)
+        nombre_participante = str(tracker.get_slot("participante"))
+        message = accionConocimiento(True, nombre_participante)
+        dispatcher.utter_message(text=message)
         return []
 
 class ActionNoGuardarConocimiento(Action):
@@ -202,7 +230,10 @@ class ActionNoGuardarConocimiento(Action):
         return "no_guardar_conocimiento"
 
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        accionConocimiento(False, dispatcher, tracker)
+        nombre_participante = str(tracker.get_slot("participante"))
+        message = accionConocimiento(False, nombre_participante)
+        dispatcher.utter_message(text=message)
         return []
+
 
 
